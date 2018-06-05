0d34275c364d7d7bb425e7987f06bf1260c529b0
Randieu <r-stuurman@outlook.com>
2018-06-05 16:26:36 +0200

    Player movement more object focussed and sizing

--- src/dungeonGame/entitites/creatures/Creature.java
+++ src/dungeonGame/entitites/creatures/Creature.java
@@ -4,12 +4,62 @@ import dungeonGame.entitites.Entity;
 
 public abstract class Creature extends Entity{
 
-	protected int health;
+	//default stats, this can be changed however
+	public static final int DEFAULT_HEALTH = 10;
+	public static final float DEAFAULT_SPEED = 3.0f;
+	public static final int DEFAULT_CREATURE_WIDTH = 64,
+							DEFAULT_CREATURE_HEIGHT = 64;
 	
-	public Creature(float x, float y) {
-		super(x, y);
-		health = 10;
-		
+	protected int health;
+	protected float speed;
+	protected float xMove, yMove;
+	
+	public Creature(float x, float y, int width, int height) {
+		super(x, y, width, height);
+		health = DEFAULT_HEALTH;
+		speed = DEAFAULT_SPEED;
+		xMove = 0;
+		yMove = 0;
+	}
+	
+	public void move() {
+		x += xMove;
+		y += yMove;
+	}
+	
+	
+	//GETTERS & SETTERS
+
+	public float getxMove() {
+		return xMove;
+	}
+
+	public void setxMove(float xMove) {
+		this.xMove = xMove;
+	}
+
+	public float getyMove() {
+		return yMove;
+	}
+
+	public void setyMove(float yMove) {
+		this.yMove = yMove;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public float getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(float speed) {
+		this.speed = speed;
 	}
 
 	
--- src/dungeonGame/entitites/creatures/Player.java
+++ src/dungeonGame/entitites/creatures/Player.java
@@ -10,27 +10,35 @@ public class Player extends Creature {
 	private Game game;
 	
 	public Player(Game game, float x, float y) {
-		super(x, y);
+		super(x, y, Creature.DEFAULT_CREATURE_WIDTH, Creature.DEFAULT_CREATURE_HEIGHT);
 		this.game = game;
 		
 	}
 
 	@Override
 	public void update() {
+		getInput();
+		move();
+	}
+	
+	private void getInput() {
+		xMove = 0;
+		yMove = 0;
+		
 		if(game.getKeyManager().up)
-			y -= 3;
+			yMove = -speed;
 		if(game.getKeyManager().down)
-			y += 3;
+			yMove = speed;
 		if(game.getKeyManager().left)
-			x -= 3;
+			xMove = -speed;
 		if(game.getKeyManager().right)
-			x += 3;
+			xMove = speed;
 		
 	}
 
 	@Override
 	public void render(Graphics g) {
-		g.drawImage(Assets.p_one, (int) x, (int) y, null); //convert the float variables to integers with casting because drawImage can't draw floats
+		g.drawImage(Assets.p_one, (int) x, (int) y, width, height, null); //convert the float variables to integers with casting because drawImage can't draw floats
 		
 		
 	}
